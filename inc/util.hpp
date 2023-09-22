69fca80a69a03e45e47b3997770f422c48aced54
Imre Korf <I.korf@outlook.com>
2023-09-22 19:04:12 +0200

    Added util

---
+++ inc/util.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+/**
+ * This file is meant as a collection of commonly used function calls that don't have a specific relation to any other class
+ */
--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "util.hpp"
+
+void error_callback(int code, const char* description);
+
 int main(void)
 {
     GLFWwindow* window;
+
+    glfwSetErrorCallback(&error_callback);
 
     // Initialize the library
     if (!glfwInit())
@@ -10,8 +20,7 @@ int main(void)
 
     // Create a windowed mode window and its OpenGL context
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
@@ -20,8 +29,7 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     // Loop until the user closes the window
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         // Render here
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -34,4 +42,31 @@ int main(void)
 
     glfwTerminate();
     return 0;
+}
+
+void error_callback(int code, const char* description)
+{
+    #ifdef DEBUG
+    if (code != GLFW_NO_ERROR) {
+        std::map<int, std::string> err_code = {
+            {0x00000000, "GLFW_NO_ERROR           "},
+            {0x00010001, "GLFW_NOT_INITIALIZED    "},
+            {0x00010002, "GLFW_NO_CURRENT_CONTEXT "},
+            {0x00010003, "GLFW_INVALID_ENUM       "},
+            {0x00010004, "GLFW_INVALID_VALUE      "},
+            {0x00010005, "GLFW_OUT_OF_MEMORY      "},
+            {0x00010006, "GLFW_API_UNAVAILABLE    "},
+            {0x00010007, "GLFW_VERSION_UNAVAILABLE"},
+            {0x00010008, "GLFW_PLATFORM_ERROR     "},
+            {0x00010009, "GLFW_FORMAT_UNAVAILABLE "},
+            {0x0001000A, "GLFW_NO_WINDOW_CONTEXT  "},
+        };
+
+		std::cout << "[GLFW] [ERR: " << err_code[code] << "]";
+		if (description)
+        	std::cout << ": " << description << std::endl;
+		else
+			std::cout << std::endl;
+    }
+    #endif
 }
