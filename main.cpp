09acc9c8b063d9e3bfc7593b968e84eb7cb8d071
Imre Korf <I.korf@outlook.com>
2023-09-22 23:58:54 +0200

    Made main according to opengl book

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <glad/glad.h> // include glad before glfw, glad already includes opengl headers
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -6,8 +8,16 @@
 
 #include "util.hpp"
 
-void error_callback(int code, const char* description);
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+namespace my_window {
+    constexpr size_t        height = 480;
+    constexpr size_t        width  = 640;
+    constexpr char const*   title  = "Hello World";
+};
+
+// callback defines
+void event_error_callback(int code, const char* description);
+void event_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void event_framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main(void)
 {
@@ -16,24 +26,53 @@ int main(void)
     std::cout << "Compiled against GLFW " 
         << GLFW_VERSION_MAJOR << "." << GLFW_VERSION_MINOR << "." << GLFW_VERSION_REVISION << std::endl;
 
-    glfwSetErrorCallback(&error_callback);
-
     // Initialize the library
     if (!glfwInit())
         return -1;
 
+    //*==================================
+    //* setup GLFW window hints before window creation, as otherwise they are not used
+    //*==================================
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    //*==================================
+    //* Setup opengl/glfw environment
+    //*==================================
+
     // Create a windowed mode window and its OpenGL context
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(my_window::width, my_window::height, my_window::title, NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    glfwSetKeyCallback(window, &key_callback);
-
     // Make the window's context current
     glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "[GLAD] [ERR]: Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glfwSwapInterval(1);                //
+    glViewport(0, 0, my_window::width, my_window::height); // setup initial view port here
 
+    //*==================================
+    //* Setup event callbacks
+    //*==================================
+    
+    glfwSetKeyCallback(window, &event_key_callback);
+    glfwSetFramebufferSizeCallback(window, &event_framebuffer_size_callback);
+    glfwSetErrorCallback(&event_error_callback);
+
+    //*==================================
+    //* Actual render loop happens here
+    //*==================================
+    
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
         // Render here
@@ -46,11 +85,18 @@ int main(void)
         glfwPollEvents();
     }
 
+    //*==================================
+    //* Clean up after render loop
+    //*==================================
+
     glfwTerminate();
     return 0;
 }
 
-void error_callback(int code, const char* description)
+
+//==== event callbacks ====//
+
+void event_error_callback(int code, const char* description)
 {
     #ifdef DEBUG
     if (code != GLFW_NO_ERROR) {
@@ -68,7 +114,7 @@ void error_callback(int code, const char* description)
             {0x0001000A, "GLFW_NO_WINDOW_CONTEXT  "},
         };
 
-		std::cout << "[GLFW] [ERR: " << err_code[code] << "]";
+		std::cout << "[GLFW] [ERR][" << err_code[code] << "]";
 		if (description)
         	std::cout << ": " << description << std::endl;
 		else
@@ -77,10 +123,16 @@ void error_callback(int code, const char* description)
     #endif
 }
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+void event_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     PARAM_UNUSED(scancode);
     PARAM_UNUSED(mods);
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void event_framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    PARAM_UNUSED(window);
+    glViewport(0, 0, width, height);
 }
